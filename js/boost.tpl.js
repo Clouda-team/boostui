990834ecf9f8d3c293605d7b4eb6670c6e5a96b2
zhangyuanwei <174298359@qq.com>
2015-01-30 11:51:47 +0800

    add gulpfile

---
+++ js/boost.tpl.js
@@ -0,0 +1,8 @@
+(function (window, undefined) {
+    /*!read(js/zepto.js)*/
+    /*!read(js/event.js)*/
+    /*!read(js/data.js)*/
+    /*!read(js/touch.js)*/
+    /*!read(js/widget.js)*/
+    /*!widgets*/
+})(window);
