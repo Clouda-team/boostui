e3429b471d0fa52235737c543f6774d8635ea823
dingquantracy <dingquantracy@gmail.com>
2015-04-15 15:24:16 +0800

    âcode style about global js

--- js/widget.js
+++ js/widget.js
@@ -1,14 +1,18 @@
-;
-(function ($) {
+/**
+ * 组件 类工厂
+ * @file widget.js
+ * @author zhangyuanwei
+ */
+;(function ($) {
     'use strict';
     var widgetMap = {};
     /**
      * widget 类工厂
      *
-     * @param name {string} widget名
-     * @param base {function} 父类
-     * @param prototype {object} 原型
-     * @return {function} 类构造函数
+     * @param {string} name  widget名
+     * @param {Function} base 父类
+     * @param {Object} prototype 原型
+     * @return {Function} 类构造函数
      */
     $.widget = function (name, base, prototype) {
         /**
@@ -27,10 +31,10 @@
          * 用于实现 this._super 调用
          */
         var proxiedPrototype = {};
-        var namespace = name.split(".")[0];
+        var namespace = name.split('.')[0];
 
-        name = name.split(".")[1];
-        fullName = namespace + "-" + name;
+        name = name.split('.')[1];
+        fullName = namespace + '-' + name;
 
         if (!prototype) {
             prototype = base;
@@ -44,7 +48,7 @@
                 return new constructor(options, element);
             }
 
-            //没有参数的时候用于继承时候构造原型
+            // 没有参数的时候用于继承时候构造原型
             if (arguments.length) {
                 this._createWidget(options, element);
             }
@@ -55,12 +59,14 @@
 
         // 将原型复制到代理对象上，提供 this._super 和 this._superApply 支持
         $.each(prototype, function (prop, value) {
+
             var _super;
             var _superApply;
 
             if (!$.isFunction(value)) {
                 proxiedPrototype[prop] = value;
-            } else {
+            }
+            else {
                 _super = function () {
                     return base.prototype[prop].apply(this, arguments);
                 };
@@ -74,7 +80,8 @@
 
                     try {
                         returnValue = value.apply(this, arguments);
-                    } finally {
+                    }
+                    finally {
                         this._super = __super;
                         this.__superApply = __superApply;
                     }
@@ -97,16 +104,16 @@
     };
 
 
-    $.widget.has = function(name){
+    $.widget.has = function (name) {
         return widgetMap.hasOwnProperty(name);
-    }
+    };
 
     var slice = Array.prototype.slice;
 
     /**
      * extend 复制对象属性到 target 上
      *
-     * @param target
+     * @param {Object} target 要复制到的对象
      * @return {undefined}
      */
     $.widget.extend = function (target) {
@@ -125,7 +132,8 @@
                         target[key] = $.isPlainObject(target[key]) ?
                             $.widget.extend({}, target[key], value) :
                             $.widget.extend({}, value);
-                    } else {
+                    }
+                    else {
                         // Copy everything else by reference
                         target[key] = value;
                     }
@@ -137,36 +145,35 @@
     };
 
 
-    var widget_magic = "__iqzll3wmdjthuxr_";
+    var widgetMagic = '__iqzll3wmdjthuxr_';
     /**
      * bridge 扩展Zepto.fn
      *
-     * @param name
-     * @param object
-     * @return {undefined}
+     * @param {string} name name
+     * @param {Object} constructor constructor
      */
     $.widget.bridge = function (name, constructor) {
         var fullName = constructor.prototype.widgetFullName || name;
-        var dataKey = widget_magic + fullName;
+        var dataKey = widgetMagic + fullName;
 
         $.fn[name] = function (options) {
-            var isMethodCall = typeof options === "string";
+            var isMethodCall = typeof options === 'string';
             var args = slice.call(arguments, 1);
             var returnValue = this;
 
             if (isMethodCall) {
-                //函数调用
+                // 函数调用
                 this.each(function () {
                     var $this = $(this);
                     var instance = $this.data(dataKey);
                     var methodValue;
 
-                    if (options === "instance") {
+                    if (options === 'instance') {
                         returnValue = instance;
                         return false;
                     }
 
-                    if (options === "destroy") {
+                    if (options === 'destroy') {
                         if (instance.destroy) {
                             instance.destroy();
                         }
@@ -175,14 +182,14 @@
                     }
 
                     if (!instance) {
-                        //TODO Error
-                        throw new Error("cannot call methods on " + name + " prior to initialization; " +
-                            "attempted to call method '" + options + "'");
+                        // TODO Error
+                        throw new Error('cannot call methods on ' + name + ' prior to initialization; ' +
+                            'attempted to call method "' + options + '"');
                     }
 
-                    if (!$.isFunction(instance[options]) || options.charAt(0) === "_") {
-                        //TODO Error
-                        throw new Error("no such method '" + options + "' for " + name + " widget instance");
+                    if (!$.isFunction(instance[options]) || options.charAt(0) === '_') {
+                        // TODO Error
+                        throw new Error('no such method "' + options + '" for ' + name + ' widget instance');
                     }
 
                     methodValue = instance[options].apply(instance, args);
@@ -191,9 +198,10 @@
                         return false;
                     }
                 });
-            } else {
-                //初始化
-                //支持多个初始化参数
+            }
+            else {
+                // 初始化
+                // 支持多个初始化参数
                 if (args.length) {
                     options = $.widget.extend.apply(null, [{}, options].concat(args));
                 }
@@ -202,12 +210,13 @@
                     var $this = $(this);
                     var instance = $this.data(dataKey);
                     if (instance) {
-                        //已经初始化过
+                        // 已经初始化过
                         instance.option(options || {});
                         if (instance._init) {
                             instance._init();
                         }
-                    } else {
+                    }
+                    else {
                         $this.data(dataKey, new constructor(options, this));
                     }
                 });
@@ -222,14 +231,17 @@
     /**
      * $.Widget 父类
      *
-     * @param  options
-     * @param element
-     * @class $.Widget
      */
-    $.Widget = function ( /* options, element */ ) {};
+    $.Widget = function (/* options, element */) {};
 
     $.Widget.prototype = {
         options: {},
+        /**
+         * 创建组件
+         * @private
+         * @param  {Object} options 配置
+         * @param  {HTMLElement} element DOM元素
+         */
         _createWidget: function (options, element) {
             this.element = $(element);
             this.options = $.widget.extend({},
@@ -238,35 +250,58 @@
                 options);
 
             this._create();
-            this._trigger("create", null, this._getCreateEventData());
+            this._trigger('create', null, this._getCreateEventData());
             this._init();
         },
+        /**
+         * @private
+         * @return {Object}
+         */
         _getCreateOptions: function () {
             return this.element.data(this.widgetFullName);
         },
+        /**
+         * @private
+         */
         _getCreateEventData: noop,
+        /**
+         * @private
+         */
         _create: noop,
+        /**
+         * @private
+         */
         _init: noop,
-
         destroy: function () {
             this._destroy();
-            //TODO 批量删除事件绑定好像不大好做 -_-!!
-            //    var $this = this.element;
-            //    $this.off(this.widgetName);
+            // TODO 批量删除事件绑定好像不大好做 -_-!!
+            // var $this = this.element;
+            // $this.off(this.widgetName);
         },
+        /**
+         * @private
+         */
         _destroy: noop,
-
+        /**
+         * 派发事件
+         * @private
+         * @param  {string} type 事件类型
+         * @param  {string} originalEvent 原始事件
+         * @param  {Object} data 传递的数据
+         * @return {[type]}
+         */
         _trigger: function (type, originalEvent, data) {
             var event;
             var callback = this.options[type];
 
-            type = this.widgetName + ":" + type;
+            type = this.widgetName + ':' + type;
             data = data || {};
             event = $.Event(type, {
                 originalEvent: originalEvent
             });
             this.element.trigger(event, data);
-            return !($.isFunction(callback) && callback.apply(this.element[0], [event].concat(data)) === false || event.isDefaultPrevented());
+            return !($.isFunction(callback) &&
+                callback.apply(this.element[0], [event].concat(data)) === false || event.isDefaultPrevented());
         },
         option: function (key, value) {
             var options = key;
@@ -275,13 +310,13 @@
             var i;
 
             if (arguments.length === 0) {
-                //得到所有的 options 值
+                // 得到所有的 options 值
                 return $.widget.extend({}, this.options);
             }
 
-            if (typeof key === "string") {
+            if (typeof key === 'string') {
                 options = {};
-                parts = key.split(".");
+                parts = key.split('.');
                 key = parts.shift();
                 if (parts.length) {
                     // key = "a.b.c.d"
@@ -296,7 +331,8 @@
                         return currentOpt[key] === undefined ? null : currentOpt[key];
                     }
                     currentOpt[key] = value;
-                } else {
+                }
+                else {
                     if (arguments.length === 1) {
                         return this.options[key] === undefined ? null : this.options[key];
                     }
@@ -307,6 +343,12 @@
             this._setOptions(options);
             return this;
         },
+        /**
+         * 设置options
+         * @private
+         * @param {Object} options 传入的options
+         * @return {Object} 当前对象
+         */
         _setOptions: function (options) {
             var key;
             for (key in options) {
@@ -315,6 +357,13 @@
 
             return this;
         },
+        /**
+         * 设置option
+         * @private
+         * @param {string} key key
+         * @param {Object|string|number|Function|boolean} value value
+         * @return {Object} 当前对象
+         */
         _setOption: function (key, value) {
             this.options[key] = value;
             return this;
